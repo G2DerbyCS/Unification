65b1f6f30b1f6aba690664d38f365b77ed6f749c
AhmedAhmet <ahmedahmetk@hotmail.co.uk>
2015-02-19 10:48:50 +0000

    Update scanDirectory.cs

--- source/scanDir/scanDirectory.cs
+++ source/scanDir/scanDirectory.cs
@@ -8,23 +8,34 @@ namespace scanDirectory
 {
     public class scanDirectory
     {
-
+        /*Stores the user prodivded dir to be scanned.*/
         public string sourceDir { get; set; }
+        /*Stores the type of audio file formats.*/
         public string fileFormat { get; set; }
 
-
+        /* param name =scanDir
+        Param name = file format
+        */
         public string initScanDir(string scanDir, string fileFormat)
         {
+            
             try
             {
+                /*Retreives files (fileFormat) from a directory and subdirectories (sourceDir).
+                Could use GetFiles, but Enumeratefiles starts enumerating the collection before the whole collection is return.
+                Better performance with EnumerateFiles.
+                */
                 List<string> scanFiles = Directory.EnumerateFiles(sourceDir, fileFormat, SearchOption.AllDirectories);
-
+                
+                /* Outputs to console the result of the scan. This was added for the purpose of testing.*/
                 Console.WriteLine("-- Audio Files: --");
                 foreach (string file in scanFiles)
                 {
                     Console.WriteLine(file);
                 }
             }
+            /* Exception handler
+            */
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
