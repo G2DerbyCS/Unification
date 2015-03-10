8e60b636bd78fc54285fd6e4274fdbcd3161f798
Sam Masamvi <tfsmasamvi@gmail.com>
2015-03-10 17:50:07 +0000

    Minor Tweaks
    Fixed backgroud render inconsistancy in
    ModernWindowFrameStyleResourceDictionary.xaml
    
    Added MainWindowViewViewModel class (unimplemented).
    
    Removed diagnostics code from ProgressSlider.xaml
    
    Added CompactModeState enum.

---
+++ source/View Models/MainWindowViewViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Unification.View_Models
+{
+    internal class MainWindowViewViewModel
+    {
+
+    }
+}
---
+++ source/Views/Enums/CompactModeStateEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace Unification.Views.Enums
+{
+    /// <summary>
+    /// An enum for use with views that have compact layout toggles to denote view layout toggle state.
+    /// </summary>
+    public enum CompactModeState
+    {
+        Full    = 1,
+        Off     = 0,
+        Partial = 3
+    }
+}
