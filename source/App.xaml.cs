991b54a7a9b5d0bc2bb6256ff0b27d64c310236c
sammasamvi <tfsmasamvi@gmail.com>
2015-03-31 18:12:06 +0100

    Minor Cleanup.
    Deleted \Views\MediaListView.xaml(.cs)
    
    Removed Unused MediaListView class.
    
    Moved startup logic to \app.xaml from \app.xaml.cs

--- source/App.xaml.cs
+++ source/App.xaml.cs
@@ -8,9 +8,5 @@ namespace Unification
     /// </summary>
     public partial class App : Application
     {
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            (new MainWindowView()).Show();
-        }
     }
 }
